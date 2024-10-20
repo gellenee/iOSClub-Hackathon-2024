70dca6775049a8e05b675fe49ffb781c256d57c3
gellenee <glennedelatorre@gmail.com>
2024-10-20 11:22:20 -0400

    Add files via upload

---
+++ question1/question1/question1App.swift
@@ -0,0 +1,17 @@
+//
+//  question1App.swift
+//  question1
+//
+//  Created by glenne d. dela torre on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct question1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
